7f008aeabdd165ea1a77053b9e063df0dd615d87
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-05-12 12:04:41 +0300

    Complete helloWorld, repeatString, reverestString, removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    for(let j = 0; j <= args.length - 1; j++)
+        for (let i = 0; i <= array.length - 1; i++) {
+            if(array[i] === args[j]) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
 
+    return array;
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepeats) {
+    
+    if (numberOfRepeats == -1) {
+        return "ERROR";
+    }
+    
+    let stringToRepeat = "";
 
+    for(let i = 0; i < numberOfRepeats; i++) {
+        stringToRepeat = stringToRepeat + string;
+    }
+
+    return stringToRepeat;
 }
 
 module.exports = repeatString

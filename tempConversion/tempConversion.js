dc3de88b4dd7c61c4af833bafaa0a6b44919e34f
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-05-12 12:56:38 +0300

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
+  let convertedTemperature = ((temperature - 32) * (5 / 9));
+  let roundedTemperature = Math.round(convertedTemperature * 10) / 10;
 
+  return roundedTemperature;
 }
 
-const ctof = function() {
+const ctof = function(temperature) {
+  let convertedTemperature = ((temperature * (9 / 5)) + 32);
+  let roundedTemperature = Math.round(convertedTemperature * 10) / 10;
 
+  return roundedTemperature;
 }
 
 module.exports = {

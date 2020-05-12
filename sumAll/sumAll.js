a102bb58ce414b716459de7a8f0accbf6561683e
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-05-12 12:16:40 +0300

    Complete sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if(typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    }
+    if(start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if(start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll
